3628cc78e7782a24e5672ccfd7a39debd035cdf0
Vlad Potcebnev <pocvlad@gmail.com>
2023-05-03 11:33:37 +0300

    First commit

---
+++ src/test/java/com/pocvlad/tests/JUnitTest.java
@@ -0,0 +1,44 @@
+package com.pocvlad.tests;
+
+import com.pocvlad.pages.RegistrationPage;
+import org.junit.jupiter.api.Test;
+
+
+
+public class JUnitTest {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fillFormTest() {
+        String firstName = "Alex";
+        String lastName = "Under";
+        String email = "alexunder@gmail.com";
+        String gender = "Male";
+        String phoneNumber = "7910281369";
+
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setPhoneNumber(phoneNumber)
+                .setBirthDate("11", "2", "1990")
+                .setSubjects("Maths")
+                .setHobbies("Reading")
+                .setPicture()
+                .setAddress("125  Ariznona phd")
+                .setState("NCR")
+                .setCity("Delhi")
+                .submit()
+                .verifyResultsModalAppears()
+                .verifyResult("Student Name", firstName + " " + lastName)
+                .verifyResult("Student Email", email)
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", phoneNumber)
+                .verifyResult("Date of Birth", "11 March,1990")
+                .verifyResult("Subjects", "Maths")
+                .verifyResult("Hobbies", "Reading")
+                .verifyResult("Address", "125 Ariznona phd")
+                .verifyResult("State and City", "NCR Delhi");
+    }
+}
